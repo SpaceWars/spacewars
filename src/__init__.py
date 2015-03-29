ca566001e418174a09a8001333fa47bc00f327d4
Luiz Oliveira <ziuloliveira@gmail.com>
2015-03-29 03:15:32 -0300

    Included Basic info about the project on game as the License on pydoc
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

---
+++ src/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""SpaceWars
+
+ A shooting game inspired by Space Invaders and others classic games.
+
+    Copyright (C) 2015  Luiz Fernando Oliveira, Carlos Oliveira, Matheus Souza Fernandez
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+"""
+
+# License type
+__license__ = "GPL3"
+# Date release
+__date__ = '2015'
+# Actual version
+__version__ = "0.1"
+# Authors
+__author__ = "Luiz Fernando Oliveira, Carlos Oliveira, Matheus Souza Fernandez"
