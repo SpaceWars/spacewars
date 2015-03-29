e82adf7065f8fad90285ba47716ab7766e8a712c
Luiz Oliveira <ziuloliveira@gmail.com>
2015-03-29 03:01:12 -0300

    Included some signal handlers
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
 """
 Copyright (C) 2015  SpaceWars Inc.
 
@@ -16,16 +19,40 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from cocos.layer import Layer
 from cocos.director import director
+from cocos.text import Label
 from cocos.scene import Scene
+from pyglet.window import key as Key
+import signal
 
 
 class MainWindow(Layer):
 
+    is_event_handler = True
+
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.keyboard = Key.KeyStateHandler()
+        director.window.push_handlers(self.keyboard)
 
+    def on_key_press(self, key, modifiers):
+        pass
+
+    def on_key_release(self, key, modifiers):
+        if self.keyboard[Key.ENTER]:
+            label = Label('SpaceWars',
+                          font_name='Times New Roman',
+                          font_size=32)
+            self.add(label)
+
+
+def signal_handler(signal_received, frame):
+    if signal_received is signal.SIGINT:
+        # erase the ^C on Terminal
+        print "\r  "
+        exit(0)
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
     director.init()
     main_window = MainWindow()
     main_scene = Scene(main_window)
